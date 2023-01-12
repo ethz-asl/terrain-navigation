bbd09aec90dc99e3b143bf8f2162b81bc64ebdac
Jaeyoung Lim <jalim@ethz.ch>
2023-01-12 09:52:46 +0100

    Circle goal benchmark

--- terrain_planner_benchmark/Tools/run_ics_benchmark.sh
+++ terrain_planner_benchmark/Tools/run_ics_benchmark.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
-source /opt/ros/noetic/setup.bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+OUTPUT_PATH=${SCRIPT_DIR}/../../output
+
+source /opt/ros/${ROS_DISTRO}/setup.bash
 source ~/catkin_ws/devel/setup.bash
 
-roslaunch terrain_planner_benchmark test_ics.launch location:="dischma_valley"
-roslaunch terrain_planner_benchmark test_ics.launch location:="mythen"
-roslaunch terrain_planner_benchmark test_ics.launch location:="gotthard"
+touch $OUTPUT_PATH/sargans.log
+roslaunch terrain_planner_benchmark test_ics.launch location:="sargans" > ${OUTPUT_PATH}/sargans.log
+touch $OUTPUT_PATH/dischma_valley.log
+roslaunch terrain_planner_benchmark test_ics.launch location:="dischma_valley" > ${OUTPUT_PATH}/dischma_valley.log
+touch $OUTPUT_PATH/gotthard.log
+roslaunch terrain_planner_benchmark test_ics.launch location:="gotthard" > ${OUTPUT_PATH}/gotthard.log
